20e043c2ccca781f1f2036a590e4e5e2116306b7
kang <510391707@qq.com>
2013-01-08 12:40:23 +0800

    添加一些新的文件

---
+++ module_test/TextUtil.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#Copyright (c) 2013 kangness .All rights reserved.
+
+"""
+This module provides a few string manipulation functions.
+>>>is_balanced(("Python (is (not (lisp))))")
+True
+>>>shorten("The Crossing", 10)
+'The Cro...'
+>>>simplify("some   text   with spurious whtiespace ")
+'some text with spurious whitespace'
+"""
+__author__ = 'kangness'
+
+import  string
---
+++ module_test/test_shape.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+__author__ = 'test'
+
+
+import Shape
+a = Shape.Point()
+repr(a)
+b = Shape.Point(3,4)
+str(b)
+b.distance_from_origin()
+b.x = -19
+str
+
